24ee6a15a648eed4cfa364d9244f7f1d95ae9633
ddd0010 <ricedeveloper@gmail.com>
2026-09-05 10:31:22 +0700

    Добавлен метод для вычисления значения функции

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -25,7 +25,7 @@
     static void Fibonacci()
     {
         Console.WriteLine("\nФибоначчи");
-        Console.WriteLine("Введите n (0..20): ");
+        Console.Write("Введите n (0..20): ");
 
         if (!int.TryParse(Console.ReadLine(), out int n) || n < 0 || n > 20)
         {
@@ -50,13 +50,29 @@
             b = temp + b;
         }
 
+        Console.WriteLine("");
+    }
 
+    static void ValueFunction()
+    {
+        Console.WriteLine("\nВычисление значения функции");
+        Console.Write("Введите n (-10..10): ");
+
+        if (!double.TryParse(Console.ReadLine(), out double x) || x < -10.0 || x > 10.0)
+        {
+            Console.WriteLine("Ошибка: нужно число от -10 до 10.");
+            return;
+        }
+
+        double a = Math.Sqrt(Math.Log(4.0 / 3.0)) + (x + 9.0 / 7.0) - Math.Exp(Math.Sin(1.3 * x - 0.7));
+        Console.WriteLine(a);
     }
 
     static void Main(string[] args)
     {
         Factorial();
         Fibonacci();
+        ValueFunction();
 
         Console.ReadLine();
     }
